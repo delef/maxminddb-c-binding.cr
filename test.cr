3a31bec2f2dd7701bce3a69a07a5a60d8bc34224
delef <delef@ya.ru>
2017-06-26 00:04:19 +0300

    testing

--- test.cr
+++ test.cr
@@ -1,24 +1,41 @@
 require "./src/geoip2"
 
+class Test
+  def initialize
+    @result = {names: [] of String, cities: {} of String => String}
+  end
+
+  def inner(data)
+    case data.type
+    when .ut_f8_string?
+      String.new(data.data.utf8_string, data.data_size)
+    end
+  end
+
+  def entry_data(item)
+    return unless item.has_data
+
+    case item.type
+    when GeoIP2::LibMMDB::DataType::MAP
+      # raise item.inspect
+    when GeoIP2::LibMMDB::DataType::UTF8_STRING
+      @result[:names].push inner(item).as(String)
+    end
+
+    raise @result.inspect
+
+    puts "HD=#{item.has_data} O=#{item.offset} ON=#{item.offset_to_next} T=#{item.type} I=#{inner(item)}"
+  end
+
+  def list(ptr)
+    until ptr.null?
+      entry_data(ptr.value.entry_data)
+      ptr = ptr.value.next
+    end
+  end
+end
+
 GeoIP2.open "db/GeoLite2-City.mmdb"
-ptr = GeoIP2.lookup("139.59.147.72").not_nil!
+ptr = GeoIP2.lookup("139.59.0.0").not_nil!
 
-def print_inner!(data)
-  case data.type
-  when .ut_f8_string?
-    puts String.new(data.data.utf8_string, data.data_size)
-  end
-end
-
-def print_entry_data!(data)
-  puts "HD=#{data.has_data} O=#{data.offset} ON=#{data.offset_to_next} T=#{data.type}"
-  print_inner! data
-end
-
-def print_list!(ptr)
-  until ptr.null?
-    print_entry_data! ptr.value.entry_data
-    ptr = ptr.value.next
-  end
-end
-print_list! ptr
+Test.new.list(ptr)
